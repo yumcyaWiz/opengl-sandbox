91af09bb14a1d435c5c190a327a5cd59f907d154
yumcyawiz <hashdolf1923@gmail.com>
2021-02-05 08:38:49 +0900

    implement show model example

---
+++ sandbox/model/shaders/shader.frag
@@ -0,0 +1,18 @@
+#version 330 core
+in vec3 position;
+in vec3 normal;
+in vec2 texCoords;
+
+out vec4 fragColor;
+
+uniform vec3 kd;
+uniform vec3 ks;
+uniform vec3 ka;
+uniform float shininess;
+
+uniform sampler2D diffuseTextures[100];
+uniform sampler2D specularTextures[100];
+
+void main() {
+  fragColor = vec4(kd + texture(diffuseTextures[0], texCoords).xyz, 1.0);
+}
---
+++ sandbox/model/src/model.cpp
@@ -0,0 +1,135 @@
+#include <iostream>
+#include <memory>
+
+#include "glad/glad.h"
+//
+#include "GLFW/glfw3.h"
+#include "imgui.h"
+#include "imgui_impl_glfw.h"
+#include "imgui_impl_opengl3.h"
+//
+#include "ogls/camera.hpp"
+#include "ogls/model.hpp"
+#include "ogls/shader.hpp"
+
+using namespace ogls;
+
+// globals
+std::unique_ptr<Camera> camera;
+int width = 1600;
+int height = 900;
+
+void handleInput(GLFWwindow* window, const ImGuiIO& io) {
+  // close application
+  if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+    glfwSetWindowShouldClose(window, GLFW_TRUE);
+  }
+
+  // camera movement
+  if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS) {
+    camera->move(CameraMovement::FORWARD, io.DeltaTime);
+  }
+  if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS) {
+    camera->move(CameraMovement::LEFT, io.DeltaTime);
+  }
+  if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS) {
+    camera->move(CameraMovement::BACKWARD, io.DeltaTime);
+  }
+  if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS) {
+    camera->move(CameraMovement::RIGHT, io.DeltaTime);
+  }
+
+  // camera look around
+  if (glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_RIGHT) == GLFW_PRESS) {
+    const float orbitSpeed = 1.0f;
+    camera->lookAround(orbitSpeed * io.MouseDelta.x,
+                       orbitSpeed * io.MouseDelta.y);
+  }
+}
+
+void framebufferSizeCallback([[maybe_unused]] GLFWwindow* window, int _width,
+                             int _height) {
+  width = _width;
+  height = _height;
+  glViewport(0, 0, width, height);
+}
+
+int main() {
+  // initialize glfw
+  if (!glfwInit()) {
+    std::cerr << "failed to initialize GLFW" << std::endl;
+    return EXIT_FAILURE;
+  }
+
+  // setup window and OpenGL context
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+  glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);  // required for Mac
+  glfwWindowHint(GLFW_SAMPLES, 4);                      // 4x MSAA
+  GLFWwindow* window =
+      glfwCreateWindow(width, height, "triangle", nullptr, nullptr);
+  if (!window) {
+    std::cerr << "failed to create window" << std::endl;
+    return EXIT_FAILURE;
+  }
+  glfwMakeContextCurrent(window);
+
+  glfwSetFramebufferSizeCallback(window, framebufferSizeCallback);
+
+  // initialize glad
+  if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+    std::cerr << "failed to initialize glad" << std::endl;
+    return EXIT_FAILURE;
+  }
+
+  // initialize imgui
+  IMGUI_CHECKVERSION();
+  ImGui::CreateContext();
+  ImGuiIO& io = ImGui::GetIO();
+  (void)io;
+
+  // set imgui style
+  ImGui::StyleColorsDark();
+
+  // initialize imgui backends
+  ImGui_ImplGlfw_InitForOpenGL(window, true);
+  ImGui_ImplOpenGL3_Init("#version 330 core");
+
+  // enable depth test
+  glEnable(GL_DEPTH_TEST);
+  // enable MSAA
+  glEnable(GL_MULTISAMPLE);
+
+  // load model
+  const std::string assetPath =
+      std::string(CMAKE_SOURCE_DIR) + std::string("/assets/sponza/sponza.obj");
+  Model model(assetPath);
+
+  // setup shader
+  Shader shader{"shaders/shader.vert", "shaders/shader.frag"};
+
+  // app loop
+  while (!glfwWindowShouldClose(window)) {
+    glfwPollEvents();
+
+    handleInput(window, io);
+
+    // set uniform variables
+    shader.setUniform("view", camera->computeViewMatrix());
+    shader.setUniform("projection",
+                      camera->computeProjectionMatrix(width, height));
+
+    // render
+    glClear(GL_COLOR_BUFFER_BIT);
+    model.draw(shader);
+
+    glfwSwapBuffers(window);
+  }
+
+  // exit
+  glfwDestroyWindow(window);
+  glfwTerminate();
+
+  return 0;
+}
