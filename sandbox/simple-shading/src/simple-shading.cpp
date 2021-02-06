966c574f7ccc4c697cbef6c8a3bd8d8ecd28cd9f
yumcyawiz <hashdolf1923@gmail.com>
2021-02-06 18:00:39 +0900

    rename window name

--- sandbox/simple-shading/src/simple-shading.cpp
+++ sandbox/simple-shading/src/simple-shading.cpp
@@ -67,7 +67,7 @@ int main() {
   glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);  // required for Mac
   glfwWindowHint(GLFW_SAMPLES, 4);                      // 4x MSAA
   GLFWwindow* window =
-      glfwCreateWindow(width, height, "triangle", nullptr, nullptr);
+      glfwCreateWindow(width, height, "simple-shading", nullptr, nullptr);
   if (!window) {
     std::cerr << "failed to create window" << std::endl;
     return EXIT_FAILURE;
