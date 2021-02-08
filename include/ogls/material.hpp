4a74a5064d33411179f2d73f7cc114c44393ff4f
yumcyawiz <hashdolf1923@gmail.com>
2021-02-09 06:37:18 +0900

    separate material to material.hpp

---
+++ include/ogls/material.hpp
@@ -0,0 +1,32 @@
+#ifndef _OGLS_MATERIAL_H
+#define _OGLS_MATERIAL_H
+#include <optional>
+
+#include "glm/glm.hpp"
+
+namespace ogls {
+
+struct Material {
+  glm::vec3 kd;  // diffuse color
+  glm::vec3 ks;  // specular color
+  glm::vec3 ka;  // ambient color
+  glm::vec3 ke;  // emissive color
+  float shininess;
+
+  std::optional<unsigned int> diffuseMap;    // index of diffuse map texture
+  std::optional<unsigned int> specularMap;   // index of specular map texture
+  std::optional<unsigned int> ambientMap;    // index of ambient map texture
+  std::optional<unsigned int> emissiveMap;   // index of emissive map texture
+  std::optional<unsigned int> heightMap;     // index of height map texture
+  std::optional<unsigned int> normalMap;     // index of normal map texture
+  std::optional<unsigned int> shininessMap;  // index of shininess map texture
+  std::optional<unsigned int>
+      displacementMap;                   // index of displacement map texture
+  std::optional<unsigned int> lightMap;  // index of light map texture
+
+  Material() : kd{0.0f}, ks{0.0f}, ka{0.0f}, ke{0.0f}, shininess{0.0f} {}
+};
+
+}  // namespace ogls
+
+#endif
--- include/ogls/mesh.hpp
+++ include/ogls/mesh.hpp
@@ -1,11 +1,11 @@
 #ifndef _OGLS_MESH_H
 #define _OGLS_MESH_H
-#include <optional>
 #include <vector>
 
 #include "glad/glad.h"
 #include "glm/glm.hpp"
 //
+#include "material.hpp"
 #include "shader.hpp"
 #include "texture.hpp"
 
@@ -18,27 +18,6 @@ struct Vertex {
   glm::vec3 tangent;    // tangent vector
 
   Vertex() : position{0.0f}, normal{0.0f}, texcoords{0.0f}, tangent{0.0f} {}
-};
-
-struct Material {
-  glm::vec3 kd;  // diffuse color
-  glm::vec3 ks;  // specular color
-  glm::vec3 ka;  // ambient color
-  glm::vec3 ke;  // emissive color
-  float shininess;
-
-  std::optional<unsigned int> diffuseMap;    // index of diffuse map texture
-  std::optional<unsigned int> specularMap;   // index of specular map texture
-  std::optional<unsigned int> ambientMap;    // index of ambient map texture
-  std::optional<unsigned int> emissiveMap;   // index of emissive map texture
-  std::optional<unsigned int> heightMap;     // index of height map texture
-  std::optional<unsigned int> normalMap;     // index of normal map texture
-  std::optional<unsigned int> shininessMap;  // index of shininess map texture
-  std::optional<unsigned int>
-      displacementMap;                   // index of displacement map texture
-  std::optional<unsigned int> lightMap;  // index of light map texture
-
-  Material() : kd{0.0f}, ks{0.0f}, ka{0.0f}, ke{0.0f}, shininess{8.0f} {}
 };
 
 class Mesh {
