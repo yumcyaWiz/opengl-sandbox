221847f6ca229cb7830012294dccaa58f340b5d0
yumcyawiz <hashdolf1923@gmail.com>
2021-02-08 15:54:54 +0900

    add other texture map members

--- include/ogls/mesh.hpp
+++ include/ogls/mesh.hpp
@@ -25,16 +25,18 @@ struct Material {
   glm::vec3 ka;  // ambient color
   float shininess;
 
-  std::optional<unsigned int> diffuseMap;   // index of diffuse map texture
-  std::optional<unsigned int> specularMap;  // index of specular map texture
+  std::optional<unsigned int> diffuseMap;    // index of diffuse map texture
+  std::optional<unsigned int> specularMap;   // index of specular map texture
+  std::optional<unsigned int> ambientMap;    // index of ambient map texture
+  std::optional<unsigned int> emissiveMap;   // index of emissive map texture
+  std::optional<unsigned int> heightMap;     // index of height map texture
+  std::optional<unsigned int> normalMap;     // index of normal map texture
+  std::optional<unsigned int> shininessMap;  // index of shininess map texture
+  std::optional<unsigned int>
+      displacementMap;                   // index of displacement map texture
+  std::optional<unsigned int> lightMap;  // index of light map texture
 
-  Material()
-      : kd{0.0f},
-        ks{0.0f},
-        ka{0.0f},
-        shininess{0.0f},
-        diffuseMap{std::nullopt},
-        specularMap{std::nullopt} {}
+  Material() : kd{0.0f}, ks{0.0f}, ka{0.0f}, shininess{0.0f} {}
 };
 
 class Mesh {
