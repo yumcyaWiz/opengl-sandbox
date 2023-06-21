19748f6205117d0cf0596d30a3571643f3711a05
yumcyawiz <hashdolf1923@gmail.com>
2023-06-22 02:43:54 +0900

    use sandbox-base in tangent space example

--- sandbox/tangent-space/src/tangent-space.cpp
+++ sandbox/tangent-space/src/tangent-space.cpp
@@ -1,142 +1,41 @@
 #include <filesystem>
-#include <iostream>
-#include <memory>
 
-#include "glad/glad.h"
-//
-#include "GLFW/glfw3.h"
-#include "imgui.h"
-#include "imgui_impl_glfw.h"
-#include "imgui_impl_opengl3.h"
-//
-#include "ogls.hpp"
+#include "sandbox-base.hpp"
 
-using namespace ogls;
-
-// globals
-std::unique_ptr<Camera> CAMERA;
-int WIDTH = 1600;
-int HEIGHT = 900;
-float LINE_LENGTH = 1;
-
-void handleInput(GLFWwindow *window, const ImGuiIO &io)
+namespace sandbox
 {
-    // close application
-    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
-        glfwSetWindowShouldClose(window, GLFW_TRUE);
-    }
-
-    // camera movement
-    if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS) {
-        CAMERA->move(CameraMovement::FORWARD, io.DeltaTime);
-    }
-    if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS) {
-        CAMERA->move(CameraMovement::LEFT, io.DeltaTime);
-    }
-    if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS) {
-        CAMERA->move(CameraMovement::BACKWARD, io.DeltaTime);
-    }
-    if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS) {
-        CAMERA->move(CameraMovement::RIGHT, io.DeltaTime);
-    }
-
-    // camera look around
-    if (glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_RIGHT) == GLFW_PRESS) {
-        CAMERA->lookAround(io.MouseDelta.x, io.MouseDelta.y);
-    }
-}
-
-void framebuffer_size_callback([[maybe_unused]] GLFWwindow *window, int _width,
-                               int _height)
+class TangentSpace : public SandboxBase
 {
-    WIDTH = _width;
-    HEIGHT = _height;
-    glViewport(0, 0, WIDTH, HEIGHT);
-}
-
-int main()
-{
-    // initialize glfw
-    if (!glfwInit()) {
-        std::cerr << "failed to initialize GLFW" << std::endl;
-        return EXIT_FAILURE;
+   public:
+    TangentSpace(uint32_t width, uint32_t height) : SandboxBase(width, height)
+    {
     }
 
-    // setup window and OpenGL context
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);  // required for Mac
-    glfwWindowHint(GLFW_SAMPLES, 4);                      // 4x MSAA
-    GLFWwindow *window =
-        glfwCreateWindow(WIDTH, HEIGHT, "tangent-space", nullptr, nullptr);
-    if (!window) {
-        std::cerr << "failed to create window" << std::endl;
-        return EXIT_FAILURE;
+    void beforeRender() override
+    {
+        glEnable(GL_DEPTH_TEST);
+        glEnable(GL_MULTISAMPLE);
+
+        pipeline.loadVertexShader(
+            std::filesystem::path(CMAKE_CURRENT_SOURCE_DIR) /
+            "shaders/shader.vert");
+        pipeline.loadFragmentShader(
+            std::filesystem::path(CMAKE_CURRENT_SOURCE_DIR) /
+            "shaders/shader.frag");
+
+        tangent_space_pipeline.loadVertexShader(
+            std::filesystem::path(CMAKE_CURRENT_SOURCE_DIR) /
+            "shaders/tangent-space.vert");
+        tangent_space_pipeline.loadGeometryShader(
+            std::filesystem::path(CMAKE_CURRENT_SOURCE_DIR) /
+            "shaders/tangent-space.geom");
+        tangent_space_pipeline.loadFragmentShader(
+            std::filesystem::path(CMAKE_CURRENT_SOURCE_DIR) /
+            "shaders/tangent-space.frag");
     }
-    glfwMakeContextCurrent(window);
 
-    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
-
-    // initialize glad
-    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
-        std::cerr << "failed to initialize glad" << std::endl;
-        return EXIT_FAILURE;
-    }
-
-    // initialize imgui
-    IMGUI_CHECKVERSION();
-    ImGui::CreateContext();
-    ImGuiIO &io = ImGui::GetIO();
-    (void)io;
-
-    // set imgui style
-    ImGui::StyleColorsDark();
-
-    // initialize imgui backends
-    ImGui_ImplGlfw_InitForOpenGL(window, true);
-    ImGui_ImplOpenGL3_Init("#version 460 core");
-
-    // enable depth test
-    glEnable(GL_DEPTH_TEST);
-    // enable MSAA
-    glEnable(GL_MULTISAMPLE);
-
-    // initialize camera
-    CAMERA = std::make_unique<Camera>();
-
-    // setup scene
-    Scene scene;
-
-    // setup shader
-    Pipeline pipeline;
-    pipeline.loadVertexShader(std::filesystem::path(CMAKE_CURRENT_SOURCE_DIR) /
-                              "shaders/shader.vert");
-    pipeline.loadFragmentShader(
-        std::filesystem::path(CMAKE_CURRENT_SOURCE_DIR) /
-        "shaders/shader.frag");
-
-    Pipeline tangent_space_pipeline;
-    tangent_space_pipeline.loadVertexShader(
-        std::filesystem::path(CMAKE_CURRENT_SOURCE_DIR) /
-        "shaders/tangent-space.vert");
-    tangent_space_pipeline.loadGeometryShader(
-        std::filesystem::path(CMAKE_CURRENT_SOURCE_DIR) /
-        "shaders/tangent-space.geom");
-    tangent_space_pipeline.loadFragmentShader(
-        std::filesystem::path(CMAKE_CURRENT_SOURCE_DIR) /
-        "shaders/tangent-space.frag");
-
-    // app loop
-    while (!glfwWindowShouldClose(window)) {
-        glfwPollEvents();
-
-        // start imgui frame
-        ImGui_ImplOpenGL3_NewFrame();
-        ImGui_ImplGlfw_NewFrame();
-        ImGui::NewFrame();
-
-        // imgui
+    void runImGui() override
+    {
         ImGui::Begin("UI");
         {
             static char modelPath[100] = {"assets/sponza/sponza.obj"};
@@ -148,29 +47,57 @@ int main()
 
             ImGui::Separator();
 
-            ImGui::InputFloat("FOV", &CAMERA->fov);
-            ImGui::InputFloat("Movement Speed", &CAMERA->movement_speed);
-            ImGui::InputFloat("Look Around Speed", &CAMERA->look_around_speed);
+            ImGui::InputFloat("FOV", &camera.fov);
+            ImGui::InputFloat("Movement Speed", &camera.movement_speed);
+            ImGui::InputFloat("Look Around Speed", &camera.look_around_speed);
 
-            if (ImGui::Button("Reset Camera")) { CAMERA->reset(); }
+            if (ImGui::Button("Reset Camera")) { camera.reset(); }
 
             ImGui::Separator();
 
-            ImGui::InputFloat("Line Length", &LINE_LENGTH);
+            ImGui::InputFloat("Line Length", &line_length);
         }
         ImGui::End();
+    }
 
-        handleInput(window, io);
+    void handleInput() override
+    {
+        // close application
+        if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+            glfwSetWindowShouldClose(window, GLFW_TRUE);
+        }
 
+        // camera movement
+        if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS) {
+            camera.move(ogls::CameraMovement::FORWARD, io->DeltaTime);
+        }
+        if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS) {
+            camera.move(ogls::CameraMovement::LEFT, io->DeltaTime);
+        }
+        if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS) {
+            camera.move(ogls::CameraMovement::BACKWARD, io->DeltaTime);
+        }
+        if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS) {
+            camera.move(ogls::CameraMovement::RIGHT, io->DeltaTime);
+        }
+
+        // camera look around
+        if (glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_RIGHT) == GLFW_PRESS) {
+            camera.lookAround(io->MouseDelta.x, io->MouseDelta.y);
+        }
+    }
+
+    void render() override
+    {
         // set uniform variables
-        const glm::mat4 view = CAMERA->computeViewMatrix();
+        const glm::mat4 view = camera.computeViewMatrix();
         const glm::mat4 projection =
-            CAMERA->computeProjectionMatrix(WIDTH, HEIGHT);
+            camera.computeProjectionMatrix(width, height);
         pipeline.setUniform("view", view);
         pipeline.setUniform("projection", projection);
         tangent_space_pipeline.setUniform("view", view);
         tangent_space_pipeline.setUniform("projection", projection);
-        tangent_space_pipeline.setUniform("lineLength", LINE_LENGTH);
+        tangent_space_pipeline.setUniform("lineLength", line_length);
 
         // render
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
@@ -178,20 +105,22 @@ int main()
 
         // show tangent space
         scene.draw(tangent_space_pipeline);
-
-        // render imgui
-        ImGui::Render();
-        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
-
-        glfwSwapBuffers(window);
     }
 
-    // exit
-    ImGui_ImplOpenGL3_Shutdown();
-    ImGui_ImplGlfw_Shutdown();
-    ImGui::DestroyContext();
-    glfwDestroyWindow(window);
-    glfwTerminate();
+   private:
+    ogls::Pipeline pipeline;
+    ogls::Pipeline tangent_space_pipeline;
+
+    float line_length = 1.0f;
+};
+
+}  // namespace sandbox
+
+int main()
+{
+    sandbox::TangentSpace app(1280, 720);
+
+    app.run();
 
     return 0;
 }
